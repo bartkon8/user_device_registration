e3e49f4808b1916098b840251e21b02b855c8ae2
Bartosz Konczak <barttkon15@gmail.com>
2025-08-24 07:36:02 +0200

    Updating device and device assignment models

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,6 @@
 class Device < ApplicationRecord
     has_many :device_assignments, dependent: :destroy
     has_many :users, through: :device_assignments
-    belongs_to :user, optional: true
+
+    validates :serial_number, presence: true, uniqueness: true
 end
